012fb3674daad952e2f56391c1dcb60e4726186b
Alejandro Rodriguez <alejandro.rodriguez@htech.com>
2023-03-21 02:21:43 -0600

    Add initial folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist';
+import { CharactersModule } from './characters/infrastructure/characters.module';
+import { ComicsModule } from './comics/infrastructure/comics.module';
+import { SeriesModule } from './series/infrastructure/series.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CharactersModule, ComicsModule, SeriesModule],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+    .setTitle('MTC')
+    .setDescription('MTC')
+    .setVersion('1.0')
+    .setContact(
+      'Alejandro Rodriguez',
+      'https://www.linkedin.com/in/alejandro/',
+      'alejandrorv944@gmail.com',
+    )
+    //.addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
